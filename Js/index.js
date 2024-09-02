72b6752803527dcfdbceb8289861453eba3f0c82
juanherrera20 <juanuper2@gmail.com>
2024-09-02 00:21:34 -0500

    Version final

--- Js/index.js
+++ Js/index.js
@@ -5,4 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(data => {
             document.getElementById('navbar').innerHTML = data;
         });
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        });
 });
